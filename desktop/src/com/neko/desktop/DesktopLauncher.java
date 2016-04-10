7a7ca822475b63809903d286126e1a4dda60a852
Rgonister <1352485083@qq.com>
2016-04-11 01:06:38 +0800

    www

---
+++ desktop/src/com/neko/desktop/DesktopLauncher.java
@@ -0,0 +1,29 @@
+package com.neko.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neko.Start;
+import com.neko.config.Config;
+
+public class DesktopLauncher {
+	
+//	-----------------------WARNING--------------------------
+//						 前方黑暗领域
+//						请提前做好准备
+//	-----------------------WARNING--------------------------
+	
+
+	public static void main(String[] arg) {
+		
+		LwjglApplicationConfiguration config = init();
+		new LwjglApplication(new Start(), config);
+	}
+
+	private static LwjglApplicationConfiguration init() {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = Config.Window_Size_Height;
+		config.width = Config.Window_Size_Width;
+		config.title = Config.Title;
+		return config;
+	}
+}
