162f4b83a646f9fc535f7fc721c9345d020527b1
Rgonister <1352485083@qq.com>
2016-04-11 01:10:14 +0800

    android

---
+++ android/src/com/neko/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.neko;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.neko.Start;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Start(), config);
+	}
+}
