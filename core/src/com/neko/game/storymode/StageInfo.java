5836a3a00d417c93a28a845fc4f8e49f8a6d4969
Rgonister <1352485083@qq.com>
2016-04-11 14:31:46 +0800

    .

--- core/src/com/neko/game/storymode/StageInfo.java
+++ core/src/com/neko/game/storymode/StageInfo.java
@@ -5,16 +5,26 @@ import com.neko.system.data.DataLoader;
 public class StageInfo {
 	public String heroname;
 	public String heroimg;
-	
+
 	public int skillcost;
 	public String skilldescription;
 	public String skillimg;
-	
+
 	public String rewardtype;
 	public int rewardnumber;
-	
-	public static StageInfo getInstance(String path){
-		return (StageInfo) DataLoader.loadData(path, StageInfo.class);
+
+	public String deckString;
+	public int[] deck;
+
+	public static StageInfo getInstance(String path) {
+		StageInfo sinfo = (StageInfo) DataLoader.loadData(path, StageInfo.class);
+		if (sinfo.deckString != null && sinfo.deckString.length() > 0) {
+			String[] s = sinfo.deckString.replaceAll("{", "").replaceAll("}", "").split(",");
+			for (int i = 0; i < s.length; i++) {
+				sinfo.deck[i] = Integer.parseInt(s[i]);
+			}
+		}
+		return sinfo;
 	}
-	
+
 }
