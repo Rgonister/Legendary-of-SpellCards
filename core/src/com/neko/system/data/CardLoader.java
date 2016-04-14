49cda437b661224d9adf5cd3f4669edd926ea6cb
Rgonister <1352485083@qq.com>
2016-04-14 19:10:59 +0800

    CardLoader

--- core/src/com/neko/system/data/CardLoader.java
+++ core/src/com/neko/system/data/CardLoader.java
@@ -7,16 +7,22 @@ import java.util.List;
 import java.util.Map;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.neko.game.item.Card;
 import com.neko.game.item.CardData;
 
 public class CardLoader {
 
-	public static List<Card> load(String path) {
-
-		String[] str = Gdx.files.internal(path).readString().replaceAll(" ", "").replaceAll("\r\n", "")
-				.replaceAll("\t", "").replaceAll("\\{", "").split("\\}");
-		return loadData(str);
+	public static final String Path = "data/card/";
+	
+	public static List<Card> load() {
+		List<Card> l = new ArrayList<Card>();
+		FileHandle[] files = Gdx.files.local(Path).list();
+		for(FileHandle f:files){
+		String[] str = f.readString().replaceAll(" ", "").replaceAll("\r\n", "").replaceAll("\t", "").replaceAll("\\{", "").split("\\}");
+		l.addAll(loadData(str));
+		}
+		return l;
 	}
 
 	private static List<Card> loadData(String[] ss) {
@@ -38,10 +44,7 @@ public class CardLoader {
 					if (field.getType().equals(String.class)) {
 						field.set(c, str);
 					} else {
-						field.set(c, Integer.parseInt(str));// only for
-															// string & int
-															// need furthur
-															// develop
+						field.set(c, Integer.parseInt(str));
 					}
 				}
 			} catch (Exception e) {
