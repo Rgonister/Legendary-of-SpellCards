f3cf3fb15d2e12ba217164a7ad227b106a7932a5
Rgonister <1352485083@qq.com>
2016-04-15 11:23:43 +0800

    data

--- core/src/com/neko/game/item/CardData.java
+++ core/src/com/neko/game/item/CardData.java
@@ -5,11 +5,11 @@ public class CardData {
 	public String RARITY;
 	public String GROUP;
 	public String TYPE;
-	public String RACE;
+	public String RACE = "";
 	public int COST;
 	public int ATK;
 	public int LIFE;
-	public String DIS;
+	public String DIS = "";
 	public String picPath;
 	public Ability[] ability;
 }
