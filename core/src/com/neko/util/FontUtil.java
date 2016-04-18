7262dfccfb3f57f5623a10f234f7804d19ff9317
Rgonister <1352485083@qq.com>
2016-04-18 16:53:29 +0800

    .

--- core/src/com/neko/util/FontUtil.java
+++ core/src/com/neko/util/FontUtil.java
@@ -6,8 +6,6 @@ public class FontUtil {
 	public static void init() {
 
 		FontActor.addlbf("sj", 23, "sj23");
-		FontActor.addlbf("sj", 25, "sj25");
-		FontActor.addlbf("st", 23, "st23");
 		FontActor.addlbf("st", 25, "st25");
 		FontActor.addlbf("st", 30, "st30");
 		FontActor.addlbf("textur", 25, "textur25");
